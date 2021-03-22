dff8c1eaca7d6accdc851361de2be30c4bfbc6bd
Steve Horton <shorton4@student.douglascollege.ca>
2021-03-22 11:33:34 -0700

    Added fetch call, POST method, and DELETE method

--- StockService.js
+++ StockService.js
@@ -7,11 +7,26 @@ export async function getStock(){
 
 //add stock item
 export async function addStockItem(newStockItem){
-
+    const stockData = JSON.stringify(newStockItem)
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}`,
+    {
+        method: 'POST',
+        headers: {'COntent-Type': 'application/json'},
+        body: stockData
+    })
+    .then (response => response.json())
 }
 
 //delete stock item
 export async function deleteStockItem(sku){
+    const deleteStockData = JSON.stringify({"sku" : sku});
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}`,
+    {
+        method : 'DELETE',
+        headers: { 'Content-Type': 'application/json'},
+        body: deleteStockData
+    })
+    .then (response => response.json())
 
-    
+
 }
